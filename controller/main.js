fb0eee40870669652c4b53c987870e6f9730a445
mentorvikash <mentorvikash@gmail.com>
2025-11-18 23:41:46 +0530

    do heavy task with worker

--- controller/main.js
+++ controller/main.js
@@ -1,22 +1,48 @@
-const { Worker } = require("worker_threads")
 
-console.log("start my worker");
+// // ====== basic example
 
-const worker = new Worker("./worker.js", {
-    workerData: {
-        name: "vikash singh"
-    }
-})
+// const { Worker } = require("worker_threads")
 
-worker.on("message", (msg) => {
-    console.log("received message from workder", msg)
-})
+// console.log("start my worker");
 
-worker.on("error", (err) => {
-    console.log("got error from worker", err.message)
-})
+// const worker = new Worker("./worker.js", {
+//     workerData: {
+//         name: "vikash singh"
+//     }
+// })
 
-worker.on("exit", (code) => {
-    console.log("worker exit wtih this code", code)
-})
+// worker.on("message", (msg) => {
+//     console.log("received message from worker", msg)
+// })
 
+// worker.on("error", (err) => {
+//     console.log("got error from worker", err.message)
+// })
+
+// worker.on("exit", (code) => {
+//     console.log("worker exit wtih this code", code)
+// })
+
+
+// ========== Heavy task worker thread
+
+// const { Worker } = require('worker_threads')
+
+// const worker = new Worker("./worker.js")
+
+// console.log("worker thread started")
+
+// worker.on("message", (result) => {
+//     console.log("fibonachi series: ", result)
+// })
+
+// worker.on("error", (err) => {
+//     console.log("error received from worker ", err.message)
+// })
+
+// worker.on("exit", (code) => {
+//     console.log("worker exit with code: ", code)
+// })
+
+
+// ============= Creating dynamic (multiple workers)
--- controller/worker.js
+++ controller/worker.js
@@ -1,5 +1,22 @@
-const { parentPort, workerData } = require("worker_threads")
+// ====== basic example
 
-parentPort.postMessage(`Hello ${workerData.name}, from worker thread`);
+// const { parentPort, workerData } = require("worker_threads")
+
+// parentPort.postMessage(`Hello ${workerData.name}, from worker thread`);
 
 
+// ========== Heavy task worker thread
+
+// const { parentPort } = require("worker_threads")
+
+// function slowfibonacci(n) {
+//     if (n < 1) return 1
+//     return slowfibonacci(n - 1) + slowfibonacci(n - 2)
+
+// }
+
+// const result = slowfibonacci(41)
+
+// parentPort.postMessage(result)
+
+// ============= Creating dynamic (multiple workers)
